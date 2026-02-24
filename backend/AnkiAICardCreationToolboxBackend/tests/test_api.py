3222716eb1dacb1642495961baaaf17a7b208be2
johannesknecht <johannesknecht@jlkmedia.de>
2026-02-24 20:08:27 +0100

    Add basic api test.

--- backend/AnkiAICardCreationToolboxBackend/tests/test_api.py
+++ backend/AnkiAICardCreationToolboxBackend/tests/test_api.py
@@ -1,2 +1,12 @@
-def test_api():
-    assert True
+from fastapi.testclient import TestClient
+
+from ankiaicardcreationtoolboxbackend.main import (app)
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
+
